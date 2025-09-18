94b0e3be549b2113fb66a1aba5f74ce51b255d88
Bohdan Mykhalchuk <bohdan.mykhalchuk@lembergsolutions.com>
2025-09-18 17:50:59 +0300

    Added language buttons

--- translaterUaBot.py
+++ translaterUaBot.py
@@ -1,4 +1,5 @@
 from telebot import TeleBot
+from telebot import types
 import translators
 import os
 from dotenv import load_dotenv
@@ -9,19 +10,34 @@ TOKEN = os.getenv('TELEGRAM_TOKEN')
 
 bot = TeleBot(TOKEN)
 
+user_translators = {}
+
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-    print(message)
-    bot.reply_to(message, "It works!")
+    user_name = message.from_user.first_name
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    itembtn1 = types.KeyboardButton('Alibaba')
+    itembtn2 = types.KeyboardButton('Bing')
+    itembtn3 = types.KeyboardButton('Deep')
+    itembtn4 = types.KeyboardButton('Google')
+    markup.add(itembtn1, itembtn2, itembtn3, itembtn4)
+    bot.send_message(message.chat.id, f"{user_name.capitalize()} welcome to Translater! Please select which translater you want to use", reply_markup=markup)
 
-@bot.message_handler(func=lambda message: True)
+@bot.message_handler(func=lambda message: message.text in ['Alibaba', 'Bing', 'Deep', 'Google'])
+def handle_translator(message):
+    user_translators[message.from_user.id] = message.text
+    remove_markup = types.ReplyKeyboardRemove()
+    bot.reply_to(message, f"Perfect! Now we are working with {message.text} translator", reply_markup=remove_markup)
+
+@bot.message_handler(func=lambda message: message.text not in ['Alibaba', 'Bing', 'Deep', 'Google'])
 def translate_message(message):
     try:
         text_to_translate = message.text
         to_language = 'en'
+        translator = user_translators[message.from_user.id]
         translated_text = translators.translate_text(
             query_text = text_to_translate,
-            translator = 'google',
+            translator = translator.lower(),
             from_language = 'auto',
             to_language = to_language
         )
