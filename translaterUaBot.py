6e0014eb142e573bd427a82a66b18ac8e862f31e
Bohdan Mykhalchuk <bohdan.mykhalchuk@lembergsolutions.com>
2025-09-19 13:25:43 +0300

    Code styles

--- translaterUaBot.py
+++ translaterUaBot.py
@@ -21,11 +21,14 @@ languages_translate_into = {
     "Italian": "it",
 }
 
+
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
     show_main_menu(message=message)
 
-@bot.message_handler(func=lambda message: message.text in ['English', 'Spanish', 'Portuguese', 'German', 'French', 'Italian'])
+
+@bot.message_handler(
+    func=lambda message: message.text in ['English', 'Spanish', 'Portuguese', 'German', 'French', 'Italian'])
 def send_welcome(message):
     if message.from_user.id not in user_translator:
         user_translator[message.from_user.id] = {}
@@ -37,15 +40,20 @@ def send_welcome(message):
     itembtn2 = types.KeyboardButton('Bing')
     itembtn3 = types.KeyboardButton('Google')
     markup.add(itembtn1, itembtn2, itembtn3)
-    bot.send_message(message.chat.id, f"{user_name.capitalize()}, please select which translater you want to use.", reply_markup=markup)
+    bot.send_message(message.chat.id, f"{user_name.capitalize()}, please select which translater you want to use.",
+                     reply_markup=markup)
     return user_translator
+
 
 @bot.message_handler(func=lambda message: message.text in ['Baidu', 'Bing', 'Google'])
 def handle_translator(message):
     user_translator[message.from_user.id]['translator'] = message.text
     remove_markup = types.ReplyKeyboardRemove()
-    bot.reply_to(message, f"Perfect! Now we are working with {message.text} translator. Please write what do you want to translate.", reply_markup=remove_markup)
+    bot.reply_to(message,
+                 f"Perfect! Now we are working with {message.text} translator. Please write what do you want to translate.",
+                 reply_markup=remove_markup)
     return user_translator
+
 
 @bot.message_handler(func=lambda message: message.text not in ['Baidu', 'Bing', 'Google', 'Change translator'])
 def translate_message(message):
@@ -54,10 +62,10 @@ def translate_message(message):
         to_language = user_translator[message.from_user.id]['language_translate_into']
         translator = user_translator[message.from_user.id]['translator']
         translated_text = translators.translate_text(
-            query_text = text_to_translate,
-            translator = translator.lower(),
-            from_language = 'auto',
-            to_language = to_language
+            query_text=text_to_translate,
+            translator=translator.lower(),
+            from_language='auto',
+            to_language=to_language
         )
 
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
@@ -70,11 +78,13 @@ def translate_message(message):
         markup.add(itembtn1)
         bot.reply_to(message, f"Unable to translate text. Error: {e}", reply_markup=markup)
 
+
 @bot.message_handler(func=lambda message: message.text == 'Change translator')
 def start_again(message):
     remove_markup = types.ReplyKeyboardRemove()
     bot.reply_to(message, "Changing...", reply_markup=remove_markup)
     show_main_menu(message=message)
+
 
 def show_main_menu(*, message: Message) -> None:
     user_name = message.from_user.first_name
@@ -86,6 +96,9 @@ def show_main_menu(*, message: Message) -> None:
     itembtn5 = types.KeyboardButton('French')
     itembtn6 = types.KeyboardButton('Italian')
     markup.add(itembtn1, itembtn2, itembtn3, itembtn4, itembtn5, itembtn6)
-    bot.send_message(message.chat.id,f"{user_name.capitalize()}, welcome to Translater! Which language would you like to translate into?", reply_markup=markup)
+    bot.send_message(message.chat.id,
+                     f"{user_name.capitalize()}, welcome to Translater! Which language would you like to translate into?",
+                     reply_markup=markup)
+
 
 bot.infinity_polling()
